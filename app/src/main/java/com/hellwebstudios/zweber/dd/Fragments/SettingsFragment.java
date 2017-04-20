082b2ee83fd504a7fa3dc27d2ab38a1775479f35
Zack Weber <weberz3@ferris.edu>
2017-04-20 12:22:53 -0400

    -Added DashTileFragment to handle swapping the DashTiles. -Wired this frag up to the main settings fragment.

--- app/src/main/java/com/hellwebstudios/zweber/dd/Fragments/SettingsFragment.java
+++ app/src/main/java/com/hellwebstudios/zweber/dd/Fragments/SettingsFragment.java
@@ -3,16 +3,23 @@ package com.hellwebstudios.zweber.dd.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.hellwebstudios.zweber.dd.Fragments.Adventures.AdventuresFragment;
 import com.hellwebstudios.zweber.dd.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
+
+    Button btnAppSettings;
+    Button btnDashTileSettings;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -23,5 +30,33 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //button.onclicks
+        btnAppSettings = (Button) getView().findViewById(R.id.btnAppSettings);
+        btnAppSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDashTileSettings = (Button) getView().findViewById(R.id.btnDashTileSettings);
+        btnDashTileSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //DashTileFragment.
+                DashTileFragment fragment = new DashTileFragment();
+                android.support.v4.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment, "dashTileFragment");
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 }
